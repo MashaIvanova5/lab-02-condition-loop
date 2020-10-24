64265bb35c530e5cb91a1431b20e886ce4f1261e
Masha <masha20028909@mail.ru>
2020-10-24 18:55:36 +0300

    лабораторная 2

---
+++ cmake-build-debug/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/masha/labs/lab-02-condition-loop/tests/test.cpp" "/home/masha/labs/lab-02-condition-loop/cmake-build-debug/CMakeFiles/tests.dir/tests/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/masha/.hunter/_Base/9a3594a/7f32107/48401e9/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/masha/labs/lab-02-condition-loop/cmake-build-debug/CMakeFiles/template.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
